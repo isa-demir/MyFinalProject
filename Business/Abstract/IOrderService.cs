df9733236b1fd5b01b62cb7254b590813d8f6d7d
isademir <isademir.dev@gmail.com>
2023-03-29 22:47:12 +0300

    Core içinde EF kodları entegre edildi.

---
+++ Business/Abstract/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IOrderService
+    {
+        List<Order> GetAll();
+        List<Order> GetOrdersByEmployeeId(int id);
+        Order GetOrderById(int id);
+
+    }
+}
---
+++ Business/Concrete/OrderManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    internal class OrderManager : IOrderService
+    {
+        private readonly IOrderDal _orderDal;
+
+        public OrderManager(IOrderDal orderDal)
+        {
+            _orderDal = orderDal;
+        }
+
+        public List<Order> GetAll()
+        {
+            return _orderDal.GetAll();
+        }
+
+        public Order GetOrderById(int id)
+        {
+            return _orderDal.Get(p => p.OrderId == id);
+        }
+
+        public List<Order> GetOrdersByEmployeeId(int id)
+        {
+            return _orderDal.GetAll(p => p.EmployeeId == id);
+        }
+    }
+}
